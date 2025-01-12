34487e22cdcedf809c49e9ee434bda1430169b9a
asmaa <elsaeeedasmaa@gmail.com>
2025-01-12 21:40:41 +0200

    change app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:touko/features/home_screen/home_screen.dart';
 
 void main() {
   runApp(const NoteApp());
@@ -8,9 +9,10 @@ class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Text('hi')
+      theme: ThemeData.dark(),
+      home:const HomeScreen(),
     );
   }
 }
