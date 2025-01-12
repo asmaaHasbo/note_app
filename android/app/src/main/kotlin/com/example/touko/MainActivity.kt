d984adb74fedb94019a7bc7eb5fa86ca9dbfdb64
asmaa <elsaeeedasmaa@gmail.com>
2025-01-12 21:20:09 +0200

    starting my note app

---
+++ android/app/src/main/kotlin/com/example/touko/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.touko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Text('hi')
+    );
+  }
+}
+
